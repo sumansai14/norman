43a8c4e53830d57eb552c3ecb98bf2926c9d0457
Sai Suman Prayaga <suman.sai14@gmail.com>
2017-05-23 13:06:37 +0530

    added cultivator models

--- norman/models/cultivator.py
+++ norman/models/cultivator.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from bitfield import BitField
 
 import logging
 
@@ -16,4 +17,13 @@ class Cultivator(models.Model):
     """
 
     user = models.OneToOneField('norman.User')
-    adhaar_number = models.CharField(_('adhaar number'), max_length=20)
+    adhaar_number = models.CharField(_('adhaar number'), max_length=20, null=True)
+    # bank_account_number = models.CharField(_('bank account number'), null=True)
+    # ifsc_code = models.CharField()
+    has_smart_phone = models.BooleanField(default=False)
+    equipment = BitField(flags=(
+        'has_tractor', _("Tractor"),
+        'has_harvestor', _("Harvestor"),
+        'has_transplanter', _("Transplanter"),
+        'has_reaper', _("Reaper"),
+    ))
--- norman/web/frontend/farmer.py
+++ norman/web/frontend/farmer.py
@@ -4,9 +4,15 @@ from norman.web.frontend.base import BaseAuthTemplateView
 class OrganizationFarmerListView(BaseAuthTemplateView):
     """Farmers list view."""
 
-    template_name = 'norman/organization_farmer_list.html'
+    template_name = 'norman/organization/farmer_list.html'
 
     def get_context_data(self, **kwargs):
         context = super(OrganizationFarmerListView, self).get_context_data(**kwargs)
         context['farmers'] = []
         return context
+
+
+class OrganizationFarmerAddView(BaseAuthTemplateView):
+    """ View to add farmers."""
+
+    template_name = 'norman/organization/farmer_add.html'
